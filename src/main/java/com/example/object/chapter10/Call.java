ea9df4514077349732b22f502a792e674404b510
woo00oo <96woo94@naver.com>
2022-10-16 20:12:44 +0900

    중복 코드 살펴보기

---
+++ src/main/java/com/example/object/chapter10/Call.java
@@ -0,0 +1,29 @@
+package com.example.object.chapter10;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+/**
+ * 개별 통과 기간
+ */
+public class Call {
+    // 통화 시작 시간
+    private LocalDateTime from;
+
+    // 통화 종료 시간
+    private LocalDateTime to;
+
+    public Call(LocalDateTime from, LocalDateTime to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public Duration getDuration() {
+        return Duration.between(from, to);
+    }
+
+    public LocalDateTime getFrom() {
+        return this.from;
+    }
+
+}
---
+++ src/main/java/com/example/object/chapter10/Phone.java
@@ -0,0 +1,52 @@
+package com.example.object.chapter10;
+
+import com.example.object.chapter02.movie_reservation.Money;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 전화 목록
+ */
+public class Phone {
+    // 단위 요금
+    private Money amount;
+    // 단위 시간
+    private Duration seconds;
+    // 전체 통화 목록
+    private List<Call> calls = new ArrayList<>();
+
+    public Phone(Money amount, Duration seconds) {
+        this.amount = amount;
+        this.seconds = seconds;
+    }
+
+    public void call(Call call) {
+        calls.add(call);
+    }
+
+    public List<Call> getCalls() {
+        return calls;
+    }
+
+    public Money getAmount() {
+        return amount;
+    }
+
+    public Duration getSeconds() {
+        return seconds;
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for(Call call : calls) {
+            result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+        }
+
+        return result;
+
+    }
+
+}
