e0424b1307c2522c9a08894a0c6fd01b38f7d1d9
woo00oo <96woo94@naver.com>
2022-10-16 22:36:37 +0900

    중복 코드 살펴보기(새로운 요구사항 추가)

---
+++ src/main/java/com/example/object/chapter10/NightlyDiscountPhone.java
@@ -0,0 +1,60 @@
+
+package com.example.object.chapter10;
+
+import com.example.object.chapter02.movie_reservation.Money;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 심야 할인 요금제
+ */
+public class NightlyDiscountPhone {
+    private static final int LATE_NIGHT_HOUR = 22;
+
+    private Money nightlyAmount;
+    private Money regularAmount;
+    private Duration seconds;
+    private List<Call> calls = new ArrayList<>();
+
+    public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
+    }
+
+    public void call(Call call) {
+        calls.add(call);
+    }
+
+    public List<Call> getCalls() {
+        return calls;
+    }
+
+    public Money getNightlyAmount() {
+        return nightlyAmount;
+    }
+
+    public Duration getSeconds() {
+        return seconds;
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for(Call call : calls) {
+            if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+                result = result.plus(
+                        nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            } else {
+                result = result.plus(
+                        regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            }
+        }
+
+        return result;
+
+    }
+
+}
